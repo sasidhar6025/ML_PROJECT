51239f41b0ab62313a80dcb3a292ba36a6e84b3e
Sasidhar <sasi.dhar6025@gmail.com>
2023-03-11 13:53:10 +0530

    logging

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ def get_requirements(file_path):
     """
     This function will return the list of packages 
     """
-
     with open(file_path) as fb:
-        requirements=[ line.replace('\n'," ") if line != "-e ." for line in fb.readlines()]
+        requirements=[ line.replace('\n'," ") for line in fb.readlines()]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
     return requirements
 
 
 
 setup(
 
-    name='MLPROJECT'
-    version='0.0.1'
-    author='Sasi'
-    author_email='sasi.dhar6025@gmail.com'
-    packages=find_packages()
+    name='MLPROJECT',
+    version='0.0.1',
+    author='Sasi',
+    author_email='sasi.dhar6025@gmail.com',
+    packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 
 )
